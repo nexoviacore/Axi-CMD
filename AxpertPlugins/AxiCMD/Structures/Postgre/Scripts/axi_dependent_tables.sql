b3249567ebfa580393fc362e7bc79b386709a6a6
salmanagilelabs <salman.s@agile-labs.com>
2026-09-25 14:06:06 +0530

    feat: Add Axi Command Line Enable option to axpstructconfig and related tables

--- AxpertPlugins/AxiCMD/Structures/Postgre/Scripts/axi_dependent_tables.sql
+++ AxpertPlugins/AxiCMD/Structures/Postgre/Scripts/axi_dependent_tables.sql
@@ -43,17 +43,19 @@ DELETE FROM axpstructconfig WHERE propvalue1 = 'axicmdmainpage.html';
 -- Insert AxiCMD Developer Option into axpstructconfigprops table
 <<
 INSERT INTO axpstructconfigprops (axpstructconfigpropsid, cancel, sourceid, mapname, username, modifiedon, createdby, createdon, wkid, app_level, app_desc, app_slevel, cancelremarks, wfroles, configprops, propcode, description, dupchk, context, ptype, caction, chyperlink, cfields, alltstructs, alliviews, alluserroles)
-VALUES (1844770000001, 'F', 0, NULL, 'admin', '2026-09-23 12:37:37', 'admin', '2026-09-23 12:00:24', NULL, 1, 1, NULL, NULL, NULL, 'AxiCMD', 'General', 'AxiCMD Developer Option', 'configtypeAxiCMD', NULL, 'All', 'F', 'F', 'T', 'F', 'T', 'F')
+VALUES (1920660000001,'F',0,NULL,'admin','2026-09-25 12:32:04','admin','2026-09-25 12:32:04',NULL,1,1,NULL,NULL,NULL,'Axi Command Line Enable','General','This is introduced to set the flag to enable AxiCommand line in the Main page','configtypeAxi Command Line Enable',NULL,'All','F','F','F','F','F','F')
 >>
+
+
 
 -- Insert AxiCMD Developer Option into axpstructconfigproval table
 <<
 INSERT INTO axpstructconfigproval (axpstructconfigprovalid, axpstructconfigpropsid, axpstructconfigprovalrow, configvalues)
-VALUES (1844770000002, 1844770000001, 1, 'True')
+VALUES (1920660000003, 1920660000001, 1, 'true')
 >>
 
 -- Insert AxiCMD Developer Option into axpstructconfig table
 <<
 INSERT INTO axpstructconfig (axpstructconfigid,cancel,sourceid,mapname,username,modifiedon,createdby,createdon,wkid,app_level,app_desc,app_slevel,cancelremarks,wfroles,asprops,setype,props,context,propvalue1,uploadfiletype,propvalue2,propsval,alluserroles,structcaption,structname,structelements,structelements1,sfield,icolumn,sbutton,hlink,stype,userroles,dupchk,purpose)
- VALUES (1016010000022,'F',0,NULL,'admin','2026-09-24 00:00:00','admin','2026-09-24 00:00:00',NULL,1,1,NULL,NULL,NULL,'Axi CMD Option','All','General',NULL,'true',NULL,NULL,'true',NULL,'ALL Forms','ALL Forms',NULL,NULL,NULL,NULL,NULL,NULL,'All','ALL','Axi CMD OptionALL FormstrueALL',NULL) 
->
+ VALUES  (1920990000006,'F',0,NULL,'admin','2026-09-25 13:36:05','admin','2026-09-25 12:37:48',NULL,1,1,NULL,NULL,NULL,'Axi Command Line Enable','All','General',NULL,'true',NULL,NULL,'true','F',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'ALL','Axi Command Line EnableGeneralALL',NULL)
+>>
